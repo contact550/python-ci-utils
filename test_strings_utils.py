6cc2befca8b94bf96b5076cffbedc6bed7453bb5
contact550 <contact@sergeishmal.com>
2026-01-28 19:22:06 +0200

    Update test_strings_utils.py

--- test_strings_utils.py
+++ test_strings_utils.py
@@ -1,3 +1,4 @@
+import pytest
 from strings_utils import count_char, is_valid_email
 
 @pytest.mark.parametrize("string, char, expected", [
@@ -15,4 +16,5 @@ def test_count_char(string, char, expected):
 def test_is_valid_email(email, expected):
 
     assert is_valid_email(email) == expected
+
 
